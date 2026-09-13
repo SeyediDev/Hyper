3b03ae30487589d8c07d164d38d547ddd38a9427
JavadSayedi <JavadSayedi@gmail.com>
2026-09-13 15:01:37 +0330

    clean extra codes

--- src/CustomerPortal/Hyper.CustomerPortal.Api/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using Hyper.CustomerPortal.Application;
-global using MediatR;
-global using Microsoft.AspNetCore.Mvc;
-global using Neo.Endpoint.Controller;
-global using Neo.Endpoint.Controller.Base;
--- src/CustomerPortal/Hyper.CustomerPortal.Application/Common/DocumentUrlHelper.cs
+++
@@ -1,8 +0,0 @@
-namespace Hyper.CustomerPortal.Application.Common;
-
-public static class DocumentUrlHelper
-{
-    public static string? BuildDocumentUrl(int? documentId)
-        => documentId.HasValue ? $"/api/documents/{documentId.Value}/download" : null;
-}
-
