0247de1ca0b44dd07d6f51dd7647f51b1eb223c9
javadsayedi <javadsayedi@gmail.com>
2026-09-14 10:20:30 +0330

    .

--- tests/Hyper.Domain.Tests/Features/Promotions/AdvancedFormulaEvaluatorTests.cs
+++
@@ -1,8 +0,0 @@
-namespace Hyper.Domain.Tests.Features.Promotions;
-
-/// <summary>
-/// تست‌های AdvancedFormulaEvaluator برای Product.*
-/// </summary>
-public class AdvancedFormulaEvaluatorTests
-{ }
-
